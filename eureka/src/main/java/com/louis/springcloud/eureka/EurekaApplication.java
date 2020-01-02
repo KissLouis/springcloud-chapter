9239476c60375f89113001bd96ac4de6949e57ef
KissLouis <javalouis@163.com>
2020-01-02 20:10:47 +0800

    Eureka 服务发现与注册中心 update

--- eureka/src/main/java/com/louis/springcloud/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/louis/springcloud/eureka/EurekaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 //Spring Cloud 注册中心
-@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
