d37ff5e79e4523959376115b13d6979be4630291
KissLouis <javalouis@163.com>
2020-01-14 21:01:20 +0800

    Spring Cloud 创建服务消费者 - Feign update

---
+++ service-consumer-feign/src/main/java/com/louis/springcloud/service/consumer/feign/config/EurekaFeignConfig.java
@@ -0,0 +1,28 @@
+package com.louis.springcloud.service.consumer.feign.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Louis
+ * @title: EurekaFeignConfig
+ * @projectName springcloud-chapter
+ * @description: TODO
+ * @date 2020/1/13 20:59
+ */
+@Configuration
+public class EurekaFeignConfig {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
